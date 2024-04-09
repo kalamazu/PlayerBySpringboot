0bbc997a579bc9379e4fcedc70b2f6ba86dc91fb
liyang <2247885838@qq.com>
2024-04-09 11:33:08 +0800

    initial

---
+++ player/src/main/java/com/liyang/player/PlayerApplication.java
@@ -0,0 +1,13 @@
+package com.liyang.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerApplication {
+
+	public static void main(String[] args) {
+			
+	}
+
+}
